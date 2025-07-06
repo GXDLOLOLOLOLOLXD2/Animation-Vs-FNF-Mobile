a36e721208477b53df4aa57ea6429d80959db869
GXDLOLOLOLOLOLXD2 <149913660+GXDLOLOLOLOLOLXD2@users.noreply.github.com>
2025-07-06 08:13:41 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -69,7 +69,9 @@ import mobile.objects.TouchPad;
 import mobile.input.MobileInputID;
 import mobile.Util;
 
+#if VIDEOS_ALLOWED
 import MP4Handler;
+#end
 
 #if sys
 import sys.FileSystem;
@@ -316,7 +318,9 @@ class PlayState extends MusicBeatState
 
 	var vignette:FlxSprite;
 
-	var mp4Background:MP4Handler;
+	#if VIDEOS_ALLOWED
+	var mp4:MP4Handler;
+	#end
 
 	var reanimatedbfOn:Bool = ClientPrefs.reanimatedbf;
 	override public function create()
@@ -819,9 +823,11 @@ class PlayState extends MusicBeatState
 				bg.x += 75;
 				add(bg);
 			case 'animatedbg':
-
+		                #if VIDEOS_ALLOWED
 				mp4Background = new MP4Handler();
 				mp4Background.playMP4("assets/videos/animatedbg.mp4", true);
+                                #end
+
 
 				/*var videos = [];
 				trace("caching images...");
